a20b82ce40e30b2e8bbdd72d219a7a94f46bee52
hari07 <hariodukkathil@gmail.com>
2022-03-17 00:53:50 +0530

    state

--- src/App.js
+++ src/App.js
@@ -1,22 +1,9 @@
-
+import Message from './Components/message';
 import './App.css';
-import Greet from './Components/Greet';
-import Welcome from './Components/welcome';
-import Hello from './Components/Hello';
 function App() {
   return (
     <div>
-      <Greet name='Bruce' hero = 'Batman'>
-        <p>I am Batman</p>
-      </Greet>
-      <Greet name='Clark' hero = 'superman'/>
-      <Greet name='Diana' hero = 'wonder women'/>
-      <Welcome name='Bruce' />
-      <Welcome name='Clark'>
-        <p>I am the death of batman</p>
-      </Welcome>
-      <Welcome name='Diana' />
-      {/* <Hello /> */}
+      <Message></Message>
     </div>
   );
 }
