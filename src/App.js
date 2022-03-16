321ec4698ca2923857979284fdfada4213a2dada
hari07 <hariodukkathil@gmail.com>
2022-03-16 19:00:39 +0530

    jsx need

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 import Greet from './Components/Greet';
 import Welcome from './Components/welcome';
+import Hello from './Components/Hello';
 function App() {
   return (
     <div>
       <Greet />
       <Welcome />
+      <Hello />
     </div>
   );
 }
---
+++ src/Components/Hello.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Hello = ()=>{
+    return (
+
+        React.createElement('div',{id:'hello'},
+        React.createElement('h1',null,'JSX pure element'))
+        )
+}
+
+export default Hello
