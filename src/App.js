eff356965ffbc9e2850218c0767bfd6d1e1fa856
hari07 <hariodukkathil@gmail.com>
2022-03-16 18:30:10 +0530

    func and class components

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Greet from './Components/Greet';
+import Welcome from './Components/welcome';
+function App() {
+  return (
+    <div>
+      <Greet />
+      <Welcome />
+    </div>
+  );
+}
+
+export default App;
