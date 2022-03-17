a88c5f2ad3d159328b136fe0e4993778cc5c8053
hari07 <hariodukkathil@gmail.com>
2022-03-17 18:17:50 +0530

    event binding 4 meth

--- src/App.js
+++ src/App.js
@@ -5,13 +5,15 @@ import Greet from './Components/Greet';
 import Welcome from './Components/welcome';
 import FunctionClick from './Components/functionClick';
 import ClassClicked from './Components/classClicked';
+import ClickBinding from './Components/ClickBinding';
 function App() {
   return (
     <div>
       {/* <Greet name='Bruce' hero='Batman' />
       <Welcome name="micheal" hero= "jackson" /> */}
-      <FunctionClick />
-      <ClassClicked />
+      {/* <FunctionClick />
+      <ClassClicked /> */}
+      <ClickBinding />
 
     </div>
   );
