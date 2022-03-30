65d6d4886423c1aa1208da431c1f26eeb7595823
hari07 <hariodukkathil@gmail.com>
2022-03-30 19:03:09 +0530

    styling and css

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
-import List from './Components/List';
-
-
+import Styles from './Components/Styles';
+import style from './Components/style.module.css'
+import './Components/style.css'
 function App() {
   return (
     <div>
-    <List />
+      <h1 className={style.success}>success</h1>
+      <h1 className='error'>error</h1>
+      <Styles />
+      <h1></h1>
     </div>
   );
 }
