56beabafc56533f421b6a29d12606f4e75b14ac0
hari07 <hariodukkathil@gmail.com>
2022-04-07 16:59:36 +0530

    lifecycle update

--- src/Components/LifeCycleB.js
+++ src/Components/LifeCycleB.js
@@ -1,29 +1,41 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
- class LifeCycleb extends Component {
-     constructor(props) {
-       super(props)
-     
-       this.state = {
-          name: 'hari'
-       }
-       console.log('lifecycle B constructor')
-     }
-     static getDerivedStateFromProps(props,state){
-         console.log('lifecycle B getDerivedStateFromProps')
-         return null
-     }
+class LifeCycleb extends Component {
+  constructor(props) {
+    super(props);
 
-     componentDidMount(){
-         console.log('lifecycle B componentDidmount');
-     }
+    this.state = {
+      name: "hari",
+    };
+    console.log("lifecycle B constructor");
+  }
+  static getDerivedStateFromProps(props, state) {
+    console.log("lifecycle B getDerivedStateFromProps");
+    return null;
+  }
+
+  componentDidMount() {
+    console.log("lifecycle B componentDidmount");
+  }
+
+  shouldComponentUpdate() {
+    console.log("lifecycle B shouldComponentUpdate");
+    return true
+}
+
+  getSnapshotBeforeUpdate() {
+    console.log("lifecycle B getSnapshotBeforeUpdate");
+    return null
+  }
+
+  componentDidUpdate() {
+    console.log("lifecycle B componentDidUpdate");
+  }
 
   render() {
-      console.log('lifecycle B render');
-    return (
-      <div>LifeCycleb Render</div>
-    )
+    console.log("lifecycle B render");
+    return <div>LifeCycleb Render</div>;
   }
 }
 
-export default LifeCycleb
+export default LifeCycleb;
